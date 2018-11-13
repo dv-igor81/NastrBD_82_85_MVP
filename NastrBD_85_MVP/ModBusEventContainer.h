186eadf4b11665d8323e3cf20f549e04d7e905ce
Dorofeev Igor <dv.igor81@rambler.ru>
2018-11-13 15:01:44 +0300

    Перенесите управление параметрами BodBos в отдельные классы...

---
+++ NastrBD_85_MVP/ModBusEventContainer.h
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------
+#ifndef ModBusEventContainerH
+#define ModBusEventContainerH
+//---------------------------------------------------------------------------
+#include "ActionEvent.h"
+using namespace smartevents;
+//---------------------------------------------------------------------------
+class ModBusEventContainer
+{
+public:
+    //===>> Параметры ModBus
+    ActionEvent<const char*> ev_NumberOfBdChange; // Номер блока
+    ActionEvent<const char*> ev_ExpositionChange; // Экспозиция, мс
+    ActionEvent<const char*> ev_MinimumCountChange; // Минимальный счёт
+    ActionEvent<const char*> ev_MaximumCountChange; // Максимальный счёт
+    ActionEvent<const char*> ev_LevelOfOverloadChange; // Уровень перегрузки
+    ActionEvent<const char*> ev_QuantityOfIntervalChange; // Кол-во интервалов
+    ActionEvent<const char*> ev_QuantityOfLookChange; // Кол-во взгляда после
+    ActionEvent<const char*> ev_LevelOfAlarm_1Change; // Уровень тревоги 1 * 10
+    ActionEvent<const char*> ev_LevelOfAlarm_2Change; // Уровень тревоги 2 * 10
+    ActionEvent<const char*> ev_LevelOfAlarm_3Change; // Уровень тревоги 3 * 10
+    ActionEvent<const char*> ev_PhonChange; // Фон, с
+    ActionEvent<const char*> ev_DurationOfPhonChange; // Длит. подинтервала фона, мс
+    ActionEvent<const char*> ev_DurationOfAlarmChange; // Длит. сигнала тревоги
+    ActionEvent<const char*> ev_DurationOfVideoChange; // Длит. сигнала видео
+    ActionEvent<> ev_SetDefClick; // По умолчанию
+    ActionEvent<> ev_WriteClick; // Запись
+    //<<=== Параметры ModBus
+};
+//---------------------------------------------------------------------------
+#endif
